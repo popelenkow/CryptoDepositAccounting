727af9e6f7ef3194ca325e9fe7ede17da3347f30
Vladlen Popelenkov <popelenkow@gmail.com>
2024-12-26 14:11:19 +0700

    Profit

--- frontend/src/page/transactions/Table/HeadAll/TotalProfit.tsx
+++ frontend/src/page/transactions/Table/HeadAll/TotalProfit.tsx
@@ -1,17 +1,79 @@
 import { Stack, TableCell, Typography } from '@mui/material';
+import { useQuery } from '@tanstack/react-query';
 import { FC } from 'react';
+import { getInstrumentInfosOptions } from '../../../../api/backend/endpoints';
+import { GridTransactionHistoryOrderPair } from '../../../../api/backend/types';
 import { getProfitPercentColor } from '../../../../common/color';
 import { currencySymbols } from '../../../../common/currency';
 import { getGridsFunding } from '../../../../common/grid/funding';
+import { getGridRangePrices } from '../../../../common/grid/ratio';
 import { getGridsSpot } from '../../../../common/grid/spot';
 import { getGridsTotal } from '../../../../common/grid/total';
 import { getGridsTrades } from '../../../../common/grid/trade';
+import { commissions } from '../../../../common/transaction';
+import { getDecimalPrecision, roundTo } from '../../../../common/value';
 import { useGridOptionsStore } from '../../Options/store';
 import { useGridList } from '../../useGridList';
 
 export const GridsHeadAllTotalProfit: FC = () => {
   const mode = useGridOptionsStore((state) => state.mode);
   const list = useGridList();
+  const infos = useQuery(getInstrumentInfosOptions).data ?? [];
+
+  const f = () => {
+    if (infos.length === 0) return;
+
+    const res = list.map((x) => {
+      const { instrument } = x.data;
+      const historyOrders = x.data.historyOrders?.filter(
+        (x) => x.buy && x.sell,
+      );
+      if (!historyOrders || historyOrders.length === 0) {
+        return { instrument };
+      }
+      const info = infos.find((i) => i.instrument === instrument)!;
+      const highPrices = getGridRangePrices(x.data, info, 'high');
+      const lowPrices = getGridRangePrices(x.data, info, 'low');
+      const quantity = historyOrders[0].buy!.quantity;
+      const calcOrders = lowPrices.map(
+        (_, index): GridTransactionHistoryOrderPair => {
+          const decimals =
+            6 +
+            getDecimalPrecision(info.priceStep) +
+            getDecimalPrecision(info.quantityStep);
+          const commission = commissions.limit;
+
+          const buyPrice = lowPrices[index];
+          const buyCost = quantity * buyPrice;
+          const buyFee = roundTo(commission * buyCost, decimals);
+
+          const sellPrice = highPrices[index];
+          const sellCost = quantity * sellPrice;
+          const sellFee = roundTo(commission * sellCost, decimals);
+
+          return {
+            calc: true,
+            profit: 0,
+            buy: {
+              fee: buyFee,
+              price: buyPrice,
+              quantity,
+            },
+            sell: {
+              fee: sellFee,
+              price: sellPrice,
+              quantity,
+            },
+          };
+        },
+      );
+      const arr = [...historyOrders, ...calcOrders];
+      const ord = Object.groupBy(arr, (x) => x.buy!.price);
+      return { instrument, ord };
+    });
+    console.log(res);
+  };
+  f();
 
   const symbol = currencySymbols[mode];
 
